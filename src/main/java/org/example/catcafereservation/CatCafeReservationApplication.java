d5ddd08c6486febcdecfbd0f6434c5c582825822
GitHubEma-Sakai <mokyumokyumohimohi@ezweb.ne.jp>
2024-06-11 21:44:15 +0900

    プロジェクトのセットアップ

---
+++ src/main/java/org/example/catcafereservation/CatCafeReservationApplication.java
@@ -0,0 +1,13 @@
+package org.example.catcafereservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatCafeReservationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatCafeReservationApplication.class, args);
+	}
+
+}
