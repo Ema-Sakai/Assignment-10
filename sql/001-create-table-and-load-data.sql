fb8a8391f39d1b4afcea6e20e405188c0320981f
GitHubEma-Sakai <mokyumokyumohimohi@ezweb.ne.jp>
2024-06-14 00:43:03 +0900

    テスト情報が正しく登録されるように桁数チェック箇所を修正。その他細かな文字列の修正。テストデータが登録されているのを確認した。

--- sql/001-create-table-and-load-data.sql
+++ sql/001-create-table-and-load-data.sql
@@ -2,15 +2,14 @@ DROP TABLE IF EXISTS reservations;
 
 CREATE TABLE reservations (
  id INT UNSIGNED AUTO_INCREMENT,
- name VARCHAR(50) NOT NULL, --必要なのは連絡手段・情報なので、名前はニックネームでも何でもよい。
+ name VARCHAR(50) NOT NULL, -- 必要なのは連絡手段・情報なので、名前はニックネームでも何でもよい。
  reservation_date DATE NOT NULL,
  reservation_time TIME NOT NULL,
  email VARCHAR(100) NOT NULL,
  phone VARCHAR(11) NOT NULL,
  PRIMARY KEY(id),
- CHECK (phone REGEXP '^[0-9]{3}-[0-9]{4}-[0-9]{4}$') --スマホで予約を想定し、この形でなければ登録できないようにした。
+ CHECK (phone REGEXP '^[0-9]{11}$') -- スマホで予約を想定し、この形でなければ登録できないようにした。
 );
 
 INSERT INTO reservations (name, reservation_date, reservation_time, email, phone) VALUES
-("名前は何でも登録できちゃうにゃん太郎", "2024-06-13", "0:30", "test@example.com", "020-2222-2222"),
-;
+("名前はにゃんでも登録できちゃうにゃん太郎", "2024-06-13", "0:30", "test@example.com", "02022222222");
