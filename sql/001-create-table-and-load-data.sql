6beb8f0c5f277e5ea95f9fc6fa8b898014130477
GitHubEma-Sakai <mokyumokyumohimohi@ezweb.ne.jp>
2024-06-16 16:15:25 +0900

    feat:READMEの作成に伴い、SQLスクリプトの選択可能時間に制約を追加。

--- sql/001-create-table-and-load-data.sql
+++ sql/001-create-table-and-load-data.sql
@@ -1,13 +1,17 @@
 DROP TABLE IF EXISTS reservations;
 
 CREATE TABLE reservations (
- id INT UNSIGNED AUTO_INCREMENT,
+ id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
  name VARCHAR(50) NOT NULL, -- 必要なのは連絡手段・情報なので、名前はニックネームでも何でもよい。
  reservation_date DATE NOT NULL,
  reservation_time TIME NOT NULL,
+ CHECK (
+     reservation_time >= '11:00:00' AND
+     reservation_time <= '14:00:00' AND
+     MINUTE(reservation_time) IN (0, 30)
+   ),
  email VARCHAR(100) NOT NULL,
  phone VARCHAR(11) NOT NULL,
- PRIMARY KEY(id),
  CHECK (phone REGEXP '^[0-9]{11}$') -- スマホで予約を想定し、この形でなければ登録できないようにした。
 );
 
