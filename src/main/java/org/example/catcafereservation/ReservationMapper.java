8482b61953e5aecd935f5aea75abb7cfe90916b4
GitHubEma-Sakai <mokyumokyumohimohi@ezweb.ne.jp>
2024-06-29 10:04:12 +0900

    feat:Insert文をMapperに追加。

--- src/main/java/org/example/catcafereservation/ReservationMapper.java
+++ src/main/java/org/example/catcafereservation/ReservationMapper.java
@@ -1,6 +1,8 @@
 package org.example.catcafereservation;
 
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.Optional;
@@ -10,4 +12,8 @@ public interface ReservationMapper {
 
     @Select("SELECT r.* FROM reservations r INNER JOIN reservations_numbers rn ON r.id = rn.reservation_id WHERE rn.reservation_number = #{reservationNumber}")
     Optional<Reservation> findByReservationNumber(String reservationNumber);
+
+    @Insert("INSERT INTO reservations (name, reservation_date, reservation_time, email, phone) VALUES (#{name}, #{reservation_date}), #{reservation_time}), #{email}), #{phone})")
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    void insert(Reservation reservation);
 }
