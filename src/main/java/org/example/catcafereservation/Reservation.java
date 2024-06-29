0593511d9881b3883d05e405fc9b36cf7288e6af
GitHubEma-Sakai <mokyumokyumohimohi@ezweb.ne.jp>
2024-06-29 10:43:08 +0900

    feat:コンストラクタのオーバーロード対応。

--- src/main/java/org/example/catcafereservation/Reservation.java
+++ src/main/java/org/example/catcafereservation/Reservation.java
@@ -5,14 +5,14 @@ import java.time.LocalTime;
 
 public class Reservation {
 
-    private int id;
+    private Integer id;
     private String name;
     private LocalDate reservationDate;
     private LocalTime reservationTime;
     private String email;
     private String phone;
 
-    public Reservation(int id, String name, LocalDate reservationDate, LocalTime reservationTime, String email, String phone) {
+    public Reservation(Integer id, String name, LocalDate reservationDate, LocalTime reservationTime, String email, String phone) {
         this.id = id;
         this.name = name;
         this.reservationDate = reservationDate;
@@ -21,7 +21,16 @@ public class Reservation {
         this.phone = phone;
     }
 
-    public int getId() {
+    public Reservation(String name, LocalDate reservationDate, LocalTime reservationTime, String email, String phone) {
+        this.id = null;
+        this.name = name;
+        this.reservationDate = reservationDate;
+        this.reservationTime = reservationTime;
+        this.email = email;
+        this.phone = phone;
+    }
+
+    public Integer getId() {
         return id;
     }
 
--- src/main/java/org/example/catcafereservation/ReservationService.java
+++ src/main/java/org/example/catcafereservation/ReservationService.java
@@ -2,6 +2,9 @@ package org.example.catcafereservation;
 
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 @Service
 public class ReservationService {
 
@@ -15,4 +18,11 @@ public class ReservationService {
         return reservationMapper.findByReservationNumber(reservationNumber)
                 .orElseThrow(() -> new ReservationNotFoundException("お探しの予約情報は存在しません。"));
     }
+
+    public Reservation insert(String name, LocalDate reservationDate, LocalTime reservationTime, String email, String phone) {
+        Reservation user = new Reservation(name, reservationDate, reservationTime, email, phone);
+        reservationMapper.insert(user);
+        return user;
+    }
+
 }
