93f7de032ddf27df62a487ecb53b994506ed1bc8
GitHubEma-Sakai <mokyumokyumohimohi@ezweb.ne.jp>
2024-07-01 20:49:20 +0900

    feat:書き忘れていたフィールドなどを追加。

--- src/main/java/org/example/catcafereservation/ReservationResponse.java
+++ src/main/java/org/example/catcafereservation/ReservationResponse.java
@@ -1,14 +1,39 @@
 package org.example.catcafereservation;
 
+//レコードクラスへ変換もありだが、クラス拡張の可能性などを考慮し保留。
 public class ReservationResponse {
 
-    private String message;
+    private final String message;
+    private final String reservationDate;
+    private final String reservationTime;
+    private final String name;
+    private final String reservationNumber;
 
-    public ReservationResponse(String message, String yyyy年MM月dd日, String hh時mm分, String name, String reservationNumber) {
+    public ReservationResponse(String message, String reservationDate, String reservationTime, String name, String reservationNumber) {
         this.message = message;
+        this.reservationDate = reservationDate;
+        this.reservationTime = reservationTime;
+        this.name = name;
+        this.reservationNumber = reservationNumber;
     }
 
     public String getMessage() {
         return message;
+    }
+
+    public String getReservationDate() {
+        return reservationDate;
+    }
+
+    public String getReservationTime() {
+        return reservationTime;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getReservationNumber() {
+        return reservationNumber;
     }
 }
