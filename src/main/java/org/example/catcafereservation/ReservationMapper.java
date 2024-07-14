c5aff326015ca790faddd3425a69afafb311c88a
GitHubEma-Sakai <mokyumokyumohimohi@ezweb.ne.jp>
2024-07-14 16:16:57 +0900

    change:MyBatisのキャメルケース変換を有効化した。

--- src/main/java/org/example/catcafereservation/ReservationMapper.java
+++ src/main/java/org/example/catcafereservation/ReservationMapper.java
@@ -10,15 +10,6 @@ public interface ReservationMapper {
     @Select("SELECT r.id, r.name, r.reservation_date, r.reservation_time, r.email, r.phone, rn.reservation_number " +
             "FROM reservations r INNER JOIN reservations_numbers rn ON r.id = rn.reservation_id " +
             "WHERE rn.reservation_number = #{reservationNumber}")
-    @Results({
-            @Result(property = "id", column = "id"),
-            @Result(property = "name", column = "name"),
-            @Result(property = "reservationDate", column = "reservation_date"),
-            @Result(property = "reservationTime", column = "reservation_time"),
-            @Result(property = "email", column = "email"),
-            @Result(property = "phone", column = "phone"),
-            @Result(property = "reservationNumber", column = "reservation_number")
-    })
     Optional<Reservation> findByReservationNumber(String reservationNumber);
 
     @Insert("INSERT INTO reservations (name, reservation_date, reservation_time, email, phone) VALUES (#{name}, #{reservationDate}, #{reservationTime}, #{email}, #{phone})")
