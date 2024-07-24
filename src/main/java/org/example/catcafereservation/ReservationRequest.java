dad34958b1b2ee30aeebb93b3bd6b86059447115
GitHubEma-Sakai <mokyumokyumohimohi@ezweb.ne.jp>
2024-07-25 01:04:12 +0900

    change:なぜValidationGroupsとValidationOrderが必要なのかを分かり易くした。

--- src/main/java/org/example/catcafereservation/ReservationRequest.java
+++ src/main/java/org/example/catcafereservation/ReservationRequest.java
@@ -13,24 +13,24 @@ import java.time.LocalTime;
 @AllArgsConstructor
 public class ReservationRequest {
 
-    @NotBlank(message = "{reservation.name.notBlank}")
-    @Size(max = 50, message = "{reservation.name.size}")
+    @NotBlank(message = "{reservation.name.notBlank}", groups = ValidationGroups.NotBlankGroup.class)
+    @Size(max = 50, message = "{reservation.name.size}", groups = ValidationGroups.PatternGroup.class)
     private String name;
 
-    @NotNull(message = "{reservation.date.notNull}")
-    @Future(message = "{reservation.date.future}")
+    @NotNull(message = "{reservation.date.notNull}", groups = ValidationGroups.NotBlankGroup.class)
+    @Future(message = "{reservation.date.future}", groups = ValidationGroups.PatternGroup.class)
     private LocalDate reservationDate;
 
-    @NotNull(message = "{reservation.time.notNull}")
-    @ValidReservationTime(message = "{reservation.time.invalid}")
+    @NotNull(message = "{reservation.time.notNull}", groups = ValidationGroups.NotBlankGroup.class)
+    @ValidReservationTime(message = "{reservation.time.invalid}", groups = ValidationGroups.PatternGroup.class)
     private LocalTime reservationTime;
 
-    @NotBlank(message = "{reservation.email.notBlank}")
-    @Email(message = "{reservation.email.valid}")
+    @NotBlank(message = "{reservation.email.notBlank}", groups = ValidationGroups.NotBlankGroup.class)
+    @Email(message = "{reservation.email.valid}", groups = ValidationGroups.PatternGroup.class)
     private String email;
 
-    @NotBlank(message = "{reservation.phone.notBlank}")
-    @Pattern(regexp = "^[0-9]{11}$", message = "{reservation.phone.pattern}")
+    @NotBlank(message = "{reservation.phone.notBlank}", groups = ValidationGroups.NotBlankGroup.class)
+    @Pattern(regexp = "^[0-9]{11}$", message = "{reservation.phone.pattern}", groups = ValidationGroups.PatternGroup.class)
     private String phone;
 
     public Reservation convertToEntity() {
